c5edff32f30132d17d1d3bf667116eb381e65d55
Samseppiol <j_hage@live.com>
2018-01-21 12:03:57 +1100

    More on require and using modules, calling objects from json files

---
+++ learnAndUnderstandNode/section4/moreOnRequire/app.js
@@ -0,0 +1,5 @@
+// No greet js file, but will look into the greet directory and find index.js by default
+const greet = require('./greet')
+
+greet.english()
+greet.spanish()
---
+++ learnAndUnderstandNode/section4/moreOnRequire/greet/index.js
@@ -0,0 +1,9 @@
+const english = require('./english')
+const spanish = require('./spanish')
+
+
+// When the key and value have the same name you don't have to specify both values, just once.
+module.exports = {
+    english,
+    spanish
+}
